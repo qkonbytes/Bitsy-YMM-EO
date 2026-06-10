c6e85032f79b658b0bb3fee980270c2f0651e133
Bitsy <qkonbytes@gmail.com>
2026-06-10 08:17:42 +0200

    Add CORS headers to API routes

--- api/health.js
+++ api/health.js
@@ -1,3 +1,11 @@
 module.exports = function handler(req, res) {
-  res.status(200).json({ status: "Bitsy YMM API is running" });
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+
+  res.status(200).json({ status: 'Bitsy YMM API is running' });
 };
